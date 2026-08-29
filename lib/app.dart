4388af013d38ad1ed637563410e7bc80a9315c1a
abdalmqadma <abd810166@gmail.com>
2026-08-29 18:11:15 +0300

    feat: route ringing alarms to mandatory challenge

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
 import 'models/work_pattern.dart';
+import 'screens/alarm_challenge_screen.dart';
 import 'screens/main_shell.dart';
 import 'screens/setup_screen.dart';
+import 'services/alarm_service.dart';
 
 class ShiftlyApp extends StatefulWidget {
   const ShiftlyApp({super.key});
@@ -11,13 +15,66 @@ class ShiftlyApp extends StatefulWidget {
 }
 
 class _ShiftlyAppState extends State<ShiftlyApp> {
+  final navigatorKey = GlobalKey<NavigatorState>();
   WorkPattern? pattern;
   bool editing = false;
+  int? activeAlarmId;
+  StreamSubscription<dynamic>? ringingSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    ringingSubscription = Alarm.ringing.listen((alarmSet) {
+      for (final alarm in alarmSet.alarms) {
+        _openChallenge(alarm.id);
+        break;
+      }
+    });
+  }
+
+  void _openChallenge(int alarmId) {
+    if (activeAlarmId == alarmId) return;
+    activeAlarmId = alarmId;
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final navigator = navigatorKey.currentState;
+      if (navigator == null) {
+        activeAlarmId = null;
+        return;
+      }
+      navigator.push(MaterialPageRoute<void>(
+        fullscreenDialog: true,
+        builder: (_) => AlarmChallengeScreen(
+          alarmId: alarmId,
+          onCompleted: () {
+            activeAlarmId = null;
+            navigator.pop();
+          },
+        ),
+      ));
+    });
+  }
+
+  Future<void> _savePattern(WorkPattern value) async {
+    await AlarmService.replacePatternAlarms(value);
+    if (!mounted) return;
+    setState(() {
+      pattern = value;
+      editing = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    ringingSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     const violet = Color(0xFF6D4AFF);
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Shiftly',
       theme: ThemeData(
@@ -43,12 +100,7 @@ class _ShiftlyAppState extends State<ShiftlyApp> {
       builder: (context, child) =>
           Directionality(textDirection: TextDirection.rtl, child: child!),
       home: pattern == null || editing
-          ? SetupScreen(onSaved: (value) {
-              setState(() {
-                pattern = value;
-                editing = false;
-              });
-            })
+          ? SetupScreen(onSaved: _savePattern)
           : MainShell(
               pattern: pattern!,
               onEditPattern: () => setState(() => editing = true),
