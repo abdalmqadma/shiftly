7fe3804e6f10be54d9db3d3d2b75190e4c82f561
abdalmqadma <abd810166@gmail.com>
2026-08-29 18:11:14 +0300

    feat: initialize native alarm service

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Alarm.init();
   runApp(const ShiftlyApp());
 }
