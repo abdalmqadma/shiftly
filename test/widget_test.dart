f31e4858820770f435529080232778bf05fa71c3
abdalmqadma <226467043+abdalmqadma@users.noreply.github.com>
2026-09-10 04:08:43 +0300

    test: make alarm-first widget test deterministic

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shiftly/app.dart';
 
 void main() {
+  setUp(() {
+    SharedPreferences.setMockInitialValues({});
+  });
+
   testWidgets('Shiftly starts as a wake-up alarm app', (tester) async {
     await tester.pumpWidget(const ShiftlyApp());
-    await tester.pumpAndSettle();
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
 
     expect(find.text('منبّهاتك'), findsOneWidget);
     expect(find.text('الشفتات'), findsOneWidget);
