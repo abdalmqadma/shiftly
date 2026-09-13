c3400d7edc598233106ee5d6492606d71c1b8782
abdalmqadma <226467043+abdalmqadma@users.noreply.github.com>
2026-09-14 00:25:27 +0300

    feat: persist theme preference

---
+++ lib/services/theme_storage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract final class ThemeStorage {
+  static const _key = 'theme_mode';
+
+  static Future<ThemeMode> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    return switch (prefs.getString(_key)) {
+      'light' => ThemeMode.light,
+      'dark' => ThemeMode.dark,
+      _ => ThemeMode.system,
+    };
+  }
+
+  static Future<void> save(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, mode.name);
+  }
+}
